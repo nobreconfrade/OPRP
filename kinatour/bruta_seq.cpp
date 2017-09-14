9b3188984a99484b385e2ac03e8d0a1df49f0de2
nobreconfrade <willpereirabr@gmail.com>
2017-09-14 01:11:19 -0300

    walk() func walking???

--- kinatour/bruta_seq.cpp
+++ kinatour/bruta_seq.cpp
@@ -9,29 +9,97 @@ struct Position{
 
 using namespace std;
 
-void walk(Position *p,int &board,int size){
+int walk(Position *p,int &board,int size){
   //https://stackoverflow.com/questions/5008804/generating-random-integer-from-a-range
   random_device rd;
   mt19937_64 gen(rd());
   //http://en.cppreference.com/w/cpp/numeric/random/uniform_int_distribution
-  uniform_int_distribution<int> uni(0,100);
-  int random = uni(gen);
-  cout << random<<"\n";
+  uniform_real_distribution<> uni(0,99);
+  cout << uni(gen)<<"\n";
+  float random = uni(gen);
+
+  while(1){
+    if (0 <= random%100 < 12.5) { // move 1
+      if((p->x-2>=0)&&(p->y+1<size)){
+        if (board[p->x-2][p->y+1] == 0) {
+          p->x-=2;
+          p->y+=1;
+        }
+      }
+    }else if (12.5 <= random%100 < 25){ // move 2
+      if((p->x-1>=0)&&(p->y+2<size)){
+        if (board[p->x-1][p->y+2] == 0) {
+          p->x-=1;
+          p->y+=2;
+        }
+      }
+    }else if (25 <= random%100 < 37.5){ // move 3
+      if((p->x+1<size)&&(p->y+2<size)){
+        if (board[p->x+1][p->y+2] == 0) {
+          p->x+=1;
+          p->y+=2;
+        }
+      }
+    }else if (37.5 <= random%100 < 50){ // move 4
+      if((p->x+2<size)&&(p->y+1<size)){
+        if (board[p->x+2][p->y+1] == 0) {
+          p->x+=2;
+          p->y+=1;
+        }
+      }
+    }else if (50 <= random%100 < 62.5){ // move 5
+      if((p->x+2<size)&&(p->y-1>=0)){
+        if (board[p->x+2][p->y+1] == 0) {
+          p->x+=2;
+          p->y-=1;
+        }
+      }
+    }else if (62.5 <= random%100 < 75){ // move 6
+      if((p->x+1<size)&&(p->y-2>=0)){
+        if (board[p->x+1][p->y+2] == 0) {
+          p->x+=1;
+          p->y-=2;
+        }
+      }
+    }else if (75 <= random%100 < 87.5){ // move 7
+      if((p->x-1>=0)&&(p->y-2>=0)){
+        if (board[p->x-1][p->y-2] == 0) {
+          p->x-=1;
+          p->y-=2;
+        }
+      }
+    }else if (87.5 <= random%100 < 100){ // move 8
+      if((p->x-2>=0)&&(p->y-1>=0)){
+        if (board[p->x-2][p->y-1] == 0) {
+          p->x-=2;
+          p->y-=1;
+        }
+      }
+    }
+    
+  }
+
 }
 
 int main(int argc, char const *argv[]) {
-  int board [SIZEF][SIZEF];
-  int size,count;
+  int board [SIZEF][SIZEF] = {0};
+  int size,count,end;
+  int i, j;
   Position *p;
   p = (Position *) malloc(sizeof(Position));
   cin >> size;
-  p->x = 0;
-  p->y = 0;
-  board[0][0] = 1;
-  count = 2;
-  for (int i = 0; i < size; i++) {
-    for (int j = 0;j < size; j++){
-      walk(p,**board,size);
+  while (1) {
+    p->x = 0;
+    p->y = 0;
+    board[0][0] = 1;
+    count = 2;
+    for (i = 0; i < size; i++) {
+      for (j = 0;j < size; j++){
+        end = walk(p,**board,size);
+        if (end == 1) {
+
+        }
+      }
     }
   }
   return 0;
