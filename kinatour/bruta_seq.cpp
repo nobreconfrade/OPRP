07ed8ca953d703ffb11b8fddfd9b9fa1c37b92c7
nobreconfrade <willpereirabr@gmail.com>
2017-09-14 16:49:26 -0300

    so nao ta achando resposta

--- kinatour/bruta_seq.cpp
+++ kinatour/bruta_seq.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <random>
+#include <stdlib.h>
 #define SIZEF 500
 
 struct Position{
-  int x;
-  int y;
+  int x = 0;
+  int y = 0;
 };
 
 using namespace std;
@@ -17,75 +18,90 @@ int walk(Position *p,int (&board)[SIZEF][SIZEF],int size){
   uniform_real_distribution<> uni(0,99.9);
   // cout << uni(gen)<<"\n";
   double random = uni(gen);
+  int pass = 0;
   while(1){
     random = fmod(random,100);
-    int pass = 0;
-    if (0 <= random < 12.5) { // move 1
+    // cout << board[1][2]<<" BOARD[1][2]\n";
+    // cout << board[2][1]<<" BOARD[2][1]\n";
+    // cout << p->x+1<<" p.x\n";
+    // cout << p->y<<" p.y\n";
+    // if ((random >= 25)&&(random < 37.5)) {
+    //   cout << "rnd: "<<random<<"\n";
+    // }
+    if ((0 <= random)&&(random < 12.5)) { // move 1
       if((p->x-2>=0)&&(p->y+1<size)){
         if (board[p->x-2][p->y+1] == 0) {
+          // cout << "oi!1" << '\n';
           p->x-=2;
           p->y+=1;
           return 0;
         }
       }
       pass++;
-    }else if (12.5 <= random < 25){ // move 2
+    }else if ((12.5 <= random)&&(random < 25)){ // move 2
       if((p->x-1>=0)&&(p->y+2<size)){
         if (board[p->x-1][p->y+2] == 0) {
+          // cout << "oi!2" << '\n';
           p->x-=1;
           p->y+=2;
           return 0;
         }
       }
       pass++;
-    }else if (25 <= random < 37.5){ // move 3
+    }else if ((25 <= random)&&(random< 37.5)){ // move 3
       if((p->x+1<size)&&(p->y+2<size)){
         if (board[p->x+1][p->y+2] == 0) {
+          // cout << "oi!3" << '\n';
           p->x+=1;
           p->y+=2;
           return 0;
         }
       }
       pass++;
-    }else if (37.5 <= random < 50){ // move 4
+    }else if ((37.5 <= random)&&(random < 50)){ // move 4
       if((p->x+2<size)&&(p->y+1<size)){
         if (board[p->x+2][p->y+1] == 0) {
+          // cout << "oi!4" << '\n';
           p->x+=2;
           p->y+=1;
           return 0;
         }
       }
       pass++;
-    }else if (50 <= random < 62.5){ // move 5
+    }else if ((50 <= random)&&(random < 62.5)){ // move 5
       if((p->x+2<size)&&(p->y-1>=0)){
-        if (board[p->x+2][p->y+1] == 0) {
+        if (board[p->x+2][p->y-1] == 0) {
+          // cout << "oi!5" << '\n';
           p->x+=2;
           p->y-=1;
           return 0;
         }
       }
       pass++;
-    }else if (62.5 <= random < 75){ // move 6
+    }else if ((62.5 <= random)&&(random < 75)){ // move 6
       if((p->x+1<size)&&(p->y-2>=0)){
-        if (board[p->x+1][p->y+2] == 0) {
+        if (board[p->x+1][p->y-2] == 0) {
+          // cout << "oi!6" << '\n';
           p->x+=1;
           p->y-=2;
           return 0;
         }
       }
       pass++;
-    }else if (75 <= random < 87.5){ // move 7
+    }else if ((75 <= random)&&(random < 87.5)){ // move 7
       if((p->x-1>=0)&&(p->y-2>=0)){
         if (board[p->x-1][p->y-2] == 0) {
+          // cout << "oi!7" << '\n';
           p->x-=1;
           p->y-=2;
           return 0;
         }
       }
       pass++;
-    }else if (87.5 <= random < 100){ // move 8
+    }else{ // move 8
       if((p->x-2>=0)&&(p->y-1>=0)){
         if (board[p->x-2][p->y-1] == 0) {
+          // cout << "oi!8" << '\n';
           p->x-=2;
           p->y-=1;
           return 0;
@@ -93,6 +109,7 @@ int walk(Position *p,int (&board)[SIZEF][SIZEF],int size){
       }
       pass++;
     }
+
     random+=12.5;
     if (pass == 8) {
       return 1;
@@ -102,22 +119,25 @@ int walk(Position *p,int (&board)[SIZEF][SIZEF],int size){
 }
 
 int main(int argc, char const *argv[]) {
-  int board [SIZEF][SIZEF] = {0};
   int size,count,end;
+  int flag = 0, iter = 0;
   int i, j;
-  Position *p;
-  p = (Position *) malloc(sizeof(Position));
+  // vector<Position> p;
   cin >> size;
   while (1) {
-    p->x = 0;
-    p->y = 0;
+    printf("iter %i\n",iter );
+    iter++;
+    Position *p;
+    p = (Position *) malloc(sizeof(Position));
+    int board [SIZEF][SIZEF] = {0};
     board[0][0] = 1;
     count = 2;
-    for (i = 0; i < size; i++) {
+    for (i = 0; i < size; i++){
       for (j = 0;j < size; j++){
         end = walk(p,board,size);
         if (end == 1) {
-          int flag = 0;
+          flag = 0;
+          // cout << "entrou no end" << '\n';
           for (i = 0; i < count; i++) {
             for (j = 0; j < count; j++) {
               if (board[i][j] == 0) {
@@ -134,6 +154,11 @@ int main(int argc, char const *argv[]) {
         }
         board[p->x][p->y] = count;
         count++;
+      }
+      if(flag != 0){
+        free(p);
+        // printf("free p!\n" );
+        break;
       }
     }
   }
