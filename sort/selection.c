c8a3e52ebac9c904e02300a283f6f135bc29e960
nobreconfrade <willpereirabr@gmail.com>
2017-10-23 15:42:43 -0200

    inicial change on code, like test loop

--- sort/selection.c
+++ sort/selection.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
+#include <time.h>
 
-void selection_sort(int *, unsigned long);
+void selection_sort(int *, unsigned long, unsigned int);
 void imprimir_vetor(int *, unsigned long);
+void verify(int *, unsigned long);
 
 int main(int argc, char *argv[])
 {
@@ -12,45 +14,61 @@ int main(int argc, char *argv[])
 
 	int *vetor = NULL;
 	unsigned long tam, i = 0;
+	unsigned int numThread;
+	int count;
 
-	if (argc != 2) {
-		printf("%s elementos\n", argv[0]);
+	if (argc != 3) {
+		printf("%s elementos e numero de threads\n", argv[0]);
 		exit(EXIT_FAILURE);
-	}	
-	
+	}
+
 	tam = atoi(argv[1]);
+	numThread = atoi(argv[2]);
 
 	if (!(vetor = (int *) malloc(sizeof(int) * tam))) {
 		printf("Erro ao alocar memória\n");
 		exit(EXIT_FAILURE);
 	}
 
-	srand(time(NULL));		
-	for (i = 0; i < tam; i++) {
-		*(vetor + i) = random() % 10000;
+	srand(time(NULL));
+	for (count = 0; count < 10; count++) {
+		for (i = 0; i < tam; i++) {
+			*(vetor + i) = random() % 10000;
+		}
+		gettimeofday(&timevalA, NULL);
+		selection_sort(vetor, tam, numThread);
+		gettimeofday(&timevalB, NULL);
+		verify(vetor,tam);
+		printf("%lf\n", timevalB.tv_sec - timevalA.tv_sec + (timevalB.tv_usec - timevalA.tv_usec) / (double) 1000000);
+
+		//imprimir_vetor(vetor, tam);
 	}
-
-	gettimeofday(&timevalA, NULL);
-	selection_sort(vetor, tam);	
-	gettimeofday(&timevalB, NULL);
-
-	printf("%lf\n", timevalB.tv_sec - timevalA.tv_sec + (timevalB.tv_usec - timevalA.tv_usec) / (double) 1000000);
-
-	//imprimir_vetor(vetor, tam);
-
 	free(vetor);
 	return EXIT_SUCCESS;
 }
 
-void selection_sort(int *vetor, unsigned long tam)
+void verify(int *vetor, unsigned long tam){
+	int x;
+	x = vetor[0];
+	for (int i = 0; i < tam; i++) {
+		if (x > vetor[i]) {
+			printf("TA ERRADO MANO\n");
+			break;
+		}
+		x = vetor[i];
+	}
+}
+
+
+void selection_sort(int *vetor, unsigned long tam, unsigned int numThread)
 {
 	unsigned long i, j, min;
 	int aux;
-	
+
 	for (i = 0; i < tam - 1; i++) {
 		min = i;
 		for (j = i + 1; j < tam; j++) {
-			if (vetor[j] < vetor[min]) { 
+			if (vetor[j] < vetor[min]) {
          			min = j;
 			}
      		}
